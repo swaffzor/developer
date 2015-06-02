b22ccfe1673509abd7ea6a03b968c6f20629bbed
swaffzor <swafford_old_iMac@Jeremys-iMac.domain>
2015-06-02 12:06:33 -0400

    MPR shows last date/super
    also started a txt file with common SQL commands

--- missingemail.php
+++ missingemail.php
@@ -25,7 +25,7 @@
 	if (mysqli_connect_errno()) {
 	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-	
+	ml;lmkkk
 	$phours = 1 + getWeeklyHours("Jeremy Swafford", $fromDate);
 	$esql = "INSERT INTO Hours (Submitted, Date, Name, Job, Hours, Submitter, WeeklyHours) VALUES (' ', '$fromDate', 'Jeremy Swafford', '99', '1', 'Jeremy Swafford', '$phours')";
 	mysqli_query($con, $esql);
@@ -63,7 +63,7 @@
 	$thoseEmps = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM employees WHERE Name != '---Select Employee---'"));
 
 		$message .= "<h1>Missing Person Report for ".strftime("%A",strtotime($fromDate))." ".$fromDate."</h1>";
-		$message .= "<table cellspacing='10'><th>Name</th><th>Days Missing</th>";
+		$message .= "<table cellspacing='10'><th>Name</th><th>Days Missing</th><th>Last Worked</th><th>Last Supervisor</th>";
 		$employees = mysqli_query($con,"SELECT * FROM employees WHERE Status = 'Active' and id > 1 and Company != 'Sub' ORDER BY Name");
 		$empEntered = mysqli_query($con, "SELECT * FROM Hours WHERE Date = '$fromDate'");
 		while($row = mysqli_fetch_array($employees)) {
@@ -94,16 +94,32 @@
 					$daysMissing = $row['daysMissing'] + 1;
 				}
 				mysqli_query($con, "UPDATE employees SET daysMissing='$daysMissing' WHERE Name='".$empNames[$i]."'");
+				
+				//get the last date of when they worked
+				$tempLastDayWorked = mysqli_query($con, "SELECT * FROM Hours WHERE Name = '". $empNames[$i] ."' ORDER BY Date DESC LIMIT 1");
+				while($row = mysqli_fetch_array($tempLastDayWorked)) {
+					$lastDateWorked = $row['Date'];
+					$lastSupervisor = $row['Submitter'];
+				}
+				
 				if($daysMissing > 30){
-					$message .= "<tr><td>".$empNames[$i]."</td><td>$daysMissing (HAS JUST BEEN REMOVED FROM DROP DOWN)</td></tr>";
+					$message .= "<tr><td>".$empNames[$i]."</td><td>$daysMissing (HAS JUST BEEN REMOVED FROM DROP DOWN)</td>";
 					mysqli_query($con, "UPDATE employees SET Status='expired' WHERE Name='".$empNames[$i]."'");
 				}
 				else if($daysMissing > 28){
-					$message .= "<tr><td>".$empNames[$i]."</td><td>$daysMissing (will be automaticaly removed at 30)</td></tr>";
+					$message .= "<tr><td>".$empNames[$i]."</td><td>$daysMissing (will be automatically removed at 30)</td>";
 				}
 				else{
-					$message .= "<tr><td>".$empNames[$i]."</td><td>$daysMissing</td></tr>";
+					$message .= "<tr><td>".$empNames[$i]."</td><td>$daysMissing</td>";
 				}
+				
+				//show last date worked and last supervisor
+				$message .= "<td>$lastDateWorked</td><td>$lastSupervisor</td></tr>";
+			}
+			//if an employee worked this day, reset their missing day count
+			else{
+				$daysMissing = 0;
+				mysqli_query($con, "UPDATE employees SET daysMissing='$daysMissing' WHERE Name='".$row['Name']."'");
 			}
 			$fresh = true;
 			$emps[] = $empNames[$i];
