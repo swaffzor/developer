9eec528e230e17664c50b0bda141893fbca6bfd3
Jeremy Swafford <jeremy@swafford.co>
2021-06-08 12:20:44 -0400

    correct dates

--- personnel.php
+++ personnel.php
@@ -39,20 +39,20 @@
 			If you do not know the password, contact Marc Junker
 		";
 	
-	if($_COOKIE['rememberme'] != $pword){	
-		if($_POST['pword'] == ""){
-			echo $page_to_display;
-			exit();
-		}
-		elseif($_POST['pword'] == $pword){
-			//success
-		}
-		else{
-			echo "<i>Incorrect Password</i>";
-			echo $page_to_display;
-			exit();
-		}
-	}
+	// if($_COOKIE['rememberme'] != $pword){	
+	// 	if($_POST['pword'] == ""){
+	// 		echo $page_to_display;
+	// 		exit();
+	// 	}
+	// 	elseif($_POST['pword'] == $pword){
+	// 		//success
+	// 	}
+	// 	else{
+	// 		echo "<i>Incorrect Password</i>";
+	// 		echo $page_to_display;
+	// 		exit();
+	// 	}
+	// }
 ?>
 
 
@@ -260,6 +260,11 @@
 				<option value='2018'>2018</option>
 				<option value='2019'>2019</option>
 				<option value='2020'>2020</option>
+				<option value='2021'>2021</option>
+				<option value='2022'>2022</option>
+				<option value='2023'>2023</option>
+				<option value='2024'>2024</option>
+				<option value='2025'>2025</option>
 			</select></td></div>
 			
 	<tr>
@@ -323,6 +328,11 @@
 				<option value='2018'>2018</option>
 				<option value='2019'>2019</option>
 				<option value='2020'>2020</option>
+				<option value='2021'>2021</option>
+				<option value='2022'>2022</option>
+				<option value='2023'>2023</option>
+				<option value='2024'>2024</option>
+				<option value='2025'>2025</option>
 			</select></td></div>
 			<tr>
 				<td align="center">Employee</td>
